354cde30667f14e30e75ab16afe0e90097f1a506
pavanw3b <pavanw3b@gmail.com>
2018-04-19 17:04:53 +0530

    Added a few steps

--- app/MyHTMLSanitizer.py
+++ app/MyHTMLSanitizer.py
@@ -1,7 +1,14 @@
+from bs4 import BeautifulSoup
+
+
 class MyHTMLSanitizer:
 
     @staticmethod
     def sanitize(text):
         #  Add Sanitize Rules here
+        soup = BeautifulSoup(text, "html.parser")
+        [x.extract() for x in soup.findAll('script')]
+        text = str(soup)
+        #  Sanitizing ends
 
         return text
