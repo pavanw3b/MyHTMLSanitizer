a61b1252760097cb343ad4e70906caae58977632
pavanw3b <pavanw3b@gmail.com>
2018-04-20 09:56:34 +0530

    Added more and more fixes till removing only bad attrs

--- app/MyHTMLSanitizer.py
+++ app/MyHTMLSanitizer.py
@@ -2,13 +2,29 @@ from bs4 import BeautifulSoup
 
 
 class MyHTMLSanitizer:
+    def __init__(self):
+        self.bad_tags = ['script', 'form', 'input']
+        self.bad_attributes = ['onmouseover', 'onkeydown', 'onerror']
 
-    @staticmethod
-    def sanitize(text):
+    def sanitize(self, text):
         #  Add Sanitize Rules here
+        for bad_tag in self.bad_tags:
+            soup = BeautifulSoup(text, "html.parser")
+            [x.extract() for x in soup.find_all(bad_tag)]
+            text = str(soup)
+
+        #  Remove only bad attributes
         soup = BeautifulSoup(text, "html.parser")
-        [x.extract() for x in soup.findAll('script')]
+        for tag in soup.find_all():
+            for key, value in tag.attrs.items():
+                if key in self.bad_attributes:
+                    tag[key] = None  # Set to None for now since we del in dict iteration
+
+            #  Now delete the None attrs in list iteration
+            for attr in list(tag.attrs):
+                if tag[attr] is None:
+                    del tag[attr]
         text = str(soup)
-        #  Sanitizing ends
+        #  Sanitization ends here
 
         return text
--- app/views.py
+++ app/views.py
@@ -5,45 +5,71 @@ from django.contrib.auth import logout
 from .MyHTMLSanitizer import MyHTMLSanitizer
 
 
-@login_required
+# View for homepage
+@login_required  # Check if the user is logged in. Redirects to login_url per the settings.py if unauthenticated
 def index(request):
     context = {}
 
     return render(request, 'index.html', context)
 
 
+# View for Add Follow Page
+# Also handles new follower submission
 @login_required
 def add_follower(request):
 
-    if "POST" == request.method:
+    if "POST" == request.method:  # If a new follower is submitted
         name = request.POST.get('name', '')
-        NullHydFollower.objects.create(follower_name=name)
+        NullHydFollower.objects.create(follower_name=name)  # db insert
 
+    # Return list of followers for GET or after POST
     followers = NullHydFollower.objects.all()
     context = {"followers": followers}
 
     response = render(request, 'app/add-follower.html', context)
-    response['X-XSS-Protection'] = 0
-    return response
+    response['X-XSS-Protection'] = 0  # # Disable browser XSS protection for the session.
+    return response                   # # Don't disable XSS in an actual SDLC!
 
 
+# View for Blog Article page.
+# Also handles comment submitted on the article
 @login_required
 def article(request):
+    submitted = False
+    has_error = False
+    content = ""
+
     if "POST" == request.method:
+        submitted = True
         content = request.POST.get('content', '')
         # Sanitize the input data
-        content = MyHTMLSanitizer.sanitize(content)
+        sanitizer = MyHTMLSanitizer()
+        content = sanitizer.sanitize(content)
 
-        Comment.objects.create(content=content)
+        # Check if the content all malicious & sanitize returned blank. Alert user if so.
+        if 0 == len(content):
+            content = request.POST.get('content', '')
+            has_error = True
+        # If the content has at least one acceptable char
+        else:
+            Comment.objects.create(content=content)  # Insert into db
+            content = ""
 
+    # Return the list of comments on GET request or after the POST
     comments = Comment.objects.all().order_by("-id")
-    context = {"comments": comments}
+    context = {
+        "comments": comments,
+        "submitted": submitted,
+        "has_error": has_error,
+        "content": content
+    }
 
     response = render(request, 'app/article.html', context)
-    response['X-XSS-Protection'] = 0
-    return response
+    response['X-XSS-Protection'] = 0  # Disable browser XSS protection for the session.
+    return response                   # Don't disable XSS in a actual SDLC!
 
 
+# Handles logout. Uses django's default logout
 @login_required
 def logout_user(request):
     logout(request)
