c25f6a66fb10cb0acc2675d1e9fadd96c30d8b30
pavanw3b <pavanw3b@gmail.com>
2018-04-20 19:40:22 +0530

    Basic app for public

--- mysecurityutils/myhtmlsanitizer.py
+++ mysecurityutils/myhtmlsanitizer.py
@@ -8,23 +8,5 @@ class MyHTMLSanitizer:
 
     def sanitize(self, text):
         #  Add Sanitize Rules here
-        for bad_tag in self.bad_tags:
-            soup = BeautifulSoup(text, "html.parser")
-            [x.extract() for x in soup.find_all(bad_tag)]
-            text = str(soup)
-
-        #  Remove only bad attributes
-        soup = BeautifulSoup(text, "html.parser")
-        for tag in soup.find_all():
-            for key, value in tag.attrs.items():
-                if key in self.bad_attributes:
-                    tag[key] = None  # Set to None for now since we del in dict iteration
-
-            #  Now delete the None attrs in list iteration
-            for attr in list(tag.attrs):
-                if tag[attr] is None:
-                    del tag[attr]
-        text = str(soup)
-        #  Sanitization ends here
 
         return text
